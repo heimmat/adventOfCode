1b6ac6dbdeaa47e7196f62cea34fd6b88132fe79
heimmat <git.heiming@outlook.com>
2021-03-04 14:13:45 +0100

    Year 2016 Day 22 Part 2 WIP

--- src/main/kotlin/Year2016/Day22.kt
+++ src/main/kotlin/Year2016/Day22.kt
@@ -1,6 +1,7 @@
 package Year2016
 
 import Day
+import kotlin.math.abs
 
 class Day22 : Day(2016,22) {
 
@@ -18,6 +19,12 @@ class Day22 : Day(2016,22) {
         return findNodePairs(nodes).size
     }
 
+    override fun part2(): Any {
+        val dataNode = nodes.filter { it.name.endsWith("0") }.maxByOrNull { it.name.substringAfter("x").substringBefore("-").toInt() }!!
+        val targetNode = nodes.first { it.name.endsWith("x0-y0") }
+        return "\n${drawNodeMap(nodes, targetNode, dataNode)}"
+    }
+
     fun findNodePairs(listOfNodes: List<Node>): List<Pair<Node,Node>> {
         val sortedByAvail = listOfNodes.sortedByDescending { it.avail }
         return sortedByAvail.flatMap { b ->
@@ -30,12 +37,45 @@ class Day22 : Day(2016,22) {
 
     }
 
+    private fun drawNodeMap(nodes: List<Node>, targetNode: Node, dataNode: Node): String {
+        val xMax = nodes.maxOf { it.x }
+        val yMax = nodes.maxOf { it.y }
+        val builder = StringBuilder()
+        for (y in 0..yMax) {
+            for (x in 0..xMax) {
+                val node = nodes.first { it.x == x && it.y == y }
+                builder.append(node.drawSymbol(targetNode, dataNode).padStart(3))
+            }
+            builder.appendLine()
+        }
+        return builder.toString()
+    }
+
     class Node(val name: String, val size: Int, val used: Int, val avail: Int, val percent: Int) {
         init {
             if (avail != size - used) println("Avail not valid")
         }
+
+        val x = name.substringAfter("x").substringBefore("-").toInt()
+        val y = name.substringAfter("y").toInt()
+        val coordinates = x to y
+
+        infix fun manhattanTo(other: Node): Int {
+            return abs(other.x - x) + abs(other.y - y)
+        }
+
         override fun toString(): String {
             return "$name ${size}T ${used}T ${avail}T ${percent}%"
+        }
+
+        fun drawSymbol(targetNode: Node, dataNode: Node): String {
+            return when {
+                this == targetNode -> "T"
+                this == dataNode -> "G"
+                used == 0 -> "E"
+                percent > 85 -> "#"
+                else -> "."
+            }
         }
     }
 }
