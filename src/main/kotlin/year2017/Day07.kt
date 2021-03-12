352551bba5f4ee44d513cf314b37dfc517c9cbed
heimmat <git.heiming@outlook.com>
2021-03-12 13:38:07 +0100

    Year 2017 Day 07 Part 2

--- src/main/kotlin/year2017/Day07.kt
+++ src/main/kotlin/year2017/Day07.kt
@@ -11,11 +11,56 @@ class Day07 : Day(2017,7) {
         name to Node(name, weight, children)
     }.toMap()
 
+    private val rootNode by lazy { nodes.rootNode() }
+
     override fun part1(): Any {
-        return nodes.filter { node ->
-            nodes.values.map { it.children }.none { node.key in it }
-        }.entries.first().key
+        return rootNode.key
     }
 
-    data class Node(val name: String, val weight: Int, val children: List<String> = emptyList())
+    override fun part2(): Any {
+        val (unbalancedNode, targetWeight) = findUnbalancedNode(rootNode.value.toTreeNode(nodes))
+        val diff = unbalancedNode.totalWeight - targetWeight
+        return unbalancedNode.weight - diff
+    }
+
+    private fun Map<String,Node>.rootNode(): Map.Entry<String,Node> {
+        return this.filter { node ->
+            this.values.map { it.children }.none { node.key in it }
+        }.entries.first()
+    }
+
+    data class Node(val name: String, val weight: Int, val children: List<String> = emptyList()) {
+        fun toTreeNode(mapOfNodes: Map<String, Node>): TreeNode {
+            return TreeNode(name, weight, children.mapNotNull {
+                mapOfNodes[it]?.toTreeNode(mapOfNodes)
+            })
+        }
+    }
+
+
+    private fun findUnbalancedNode(root: TreeNode): Pair<TreeNode, Int> {
+        var next = root
+        var childrenByWeight: Map<Int, List<TreeNode>> = emptyMap()
+        while (!next.childrenAreBalanced) {
+            childrenByWeight = next.children.groupBy { it.totalWeight }
+            val oddChild = childrenByWeight.entries.first { it.value.size == 1 }.value.first()
+            next = oddChild
+        }
+        return next to childrenByWeight.maxByOrNull { it.value.size }!!.key
+
+    }
+
+    data class TreeNode(val name: String, val weight: Int, val children: List<TreeNode>) {
+        private val weightOfChildren by lazy {  weightOfChildren() }
+        private fun weightOfChildren(): Int = children.sumBy { it.totalWeight }
+        val totalWeight: Int by lazy { weight + weightOfChildren  }
+
+
+        val childrenAreBalanced: Boolean by lazy { children.all { it.totalWeight == weightOfChildren/children.size} }
+
+
+
+    }
+
+
 }
