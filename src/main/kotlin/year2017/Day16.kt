97f1710bb3bc78cae88562d583fa0ed4f45a9af4
heimmat <git.heiming@outlook.com>
2021-03-30 13:58:45 +0200

    Year 2017 Day 16 Part 2

--- src/main/kotlin/year2017/Day16.kt
+++ src/main/kotlin/year2017/Day16.kt
@@ -4,65 +4,80 @@ import Day
 
 class Day16 : Day(2017,16) {
 
-    val dancers = ('a'..'p').toMutableList()//mutableListOf('a', 'b', 'c', 'd', 'e')//
+    private val dancers = ('a'..'p').toList()//mutableListOf('a', 'b', 'c', 'd', 'e')//
 
-    val moves = input.asString.split(",")
+    private val moves = input.asString.split(",")
 
     override fun part1(): Any {
-        moves.forEach {
-            executeDanceMove(it)
-        }
-        return dancers.joinToString("")
+        return executeAllMoves(dancers, moves).joinToString("")
     }
 
     override fun part2(): Any {
-        repeat(1_000_000_000) {
-            moves.forEach {
-                executeDanceMove(it)
-            }
-        }
-        return dancers.joinToString("")
+        val loopBetween = detectLoop(dancers, moves)
+        return generateSequence(dancers) {
+            executeAllMoves(it, moves)
+        }.map { it.joinToString("") }.elementAt(1_000_000%loopBetween.second)
     }
 
-    fun executeDanceMove(move: String) {
+    private fun detectLoop(initialDancers: List<Char>, moves: List<String>): Pair<Int,Int> {
+        val seenStates = mutableSetOf<List<Char>>(dancers)
+        var newState = executeAllMoves(dancers, moves)
+        var iterator = 1
+        while (newState !in seenStates) {
+            seenStates.add(newState)
+            newState = executeAllMoves(newState, moves)
+            iterator++
+        }
+        return seenStates.indexOf(newState) to iterator
+    }
+
+    private fun executeAllMoves(dancers: List<Char>, moves: List<String>): List<Char>  {
+        val mutableDancers = dancers.toMutableList()
+        moves.forEach {
+            executeDanceMove(mutableDancers, it)
+        }
+        return mutableDancers
+    }
+
+    private fun executeDanceMove(dancers: MutableList<Char>, move: String) {
         when {
-            move.startsWith("s") -> spin(move.drop(1).toInt())
+            move.startsWith("s") -> dancers.spin(move.drop(1).toInt())
             move.startsWith("x") -> {
                 val a = move.substringAfter("x").substringBefore("/").toInt()
                 val b = move.substringAfter("/").toInt()
-                exchange(a,b)
+                dancers.exchange(a,b)
             }
             move.startsWith("p") -> {
                 val a = move.substringAfter("p").substringBefore("/").first()
                 val b = move.substringAfter("/").first()
-                partner(a,b)
+                dancers.partner(a,b)
             }
             else -> throw IllegalArgumentException()
         }
     }
 
-    private fun partner(a: Char, b: Char) {
-        val aIndex = dancers.indexOf(a)
-        val bIndex = dancers.indexOf(b)
-        dancers[aIndex] = b
-        dancers[bIndex] = a
+    private fun MutableList<Char>.partner(a: Char, b: Char) {
+        val aIndex = indexOf(a)
+        val bIndex = indexOf(b)
+        this[aIndex] = b
+        this[bIndex] = a
     }
 
-    private fun exchange(a: Int, b: Int) {
-        val tmp = dancers[a]
-        dancers[a] = dancers[b]
-        dancers[b] = tmp
+    private fun MutableList<Char>.exchange(a: Int, b: Int) {
+        val tmp = this[a]
+        this[a] = this[b]
+        this[b] = tmp
     }
 
-    private fun spin(size: Int) {
-        repeat(size%dancers.size) {
+    private fun MutableList<Char>.spin(size: Int) {
+        repeat(size%this.size) {
             spin()
         }
     }
 
-    private fun spin() {
-        val last = dancers.removeLast()
-        dancers.add(0, last)
+    private fun MutableList<Char>.spin() {
+        val last = this.removeLast()
+        this.add(0, last)
     }
 
 }
