80b405da3723569e48a4346b7f8f255f322ff3d9
heimmat <git.heiming@outlook.com>
2021-04-07 14:20:51 +0200

    Year 2017 Day 18 Part 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("com.github.ajalt.clikt:clikt:3.1.0")
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.4.3")
 
 
 }
--- src/main/kotlin/year2017/Day18.kt
+++ src/main/kotlin/year2017/Day18.kt
@@ -1,6 +1,9 @@
 package year2017
 
 import Day
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
 
 class Day18 : Day(2017,18) {
 
@@ -23,26 +26,70 @@ class Day18 : Day(2017,18) {
     }
 
     override fun part1(): Any {
-        return SoundComputer().runUntilRecovered(program)
+        return SoundComputer().runUntilHalt(program)
     }
 
-    class SoundComputer {
+    @ExperimentalCoroutinesApi
+    override fun part2(): Any = runBlocking {
+//        val program = """snd 1
+//            snd 2
+//            snd p
+//            rcv a
+//            rcv b
+//            rcv c
+//            rcv d""".trimIndent().split("\n").map { SoundComputer.Command.parseString(it.trim()) }
 
-        private val register = mutableMapOf<Char, Long>()
+        var counter = 0
+        val queue0 = Channel<Long>(UNLIMITED)
+        val queue1 = Channel<Long>(UNLIMITED)
+        val computer0 = DuetComputer(0, queue0, queue1)
+        val computer1 = DuetComputer(1, queue1, queue0) {
+            counter++
+        }
+        var pointer0 = 0
+        var pointer1 = 0
+
+
+
+        val suspend1 = async {
+            while (!(computer0.isWaiting && computer1.isWaiting) && pointer0 in program.indices) {
+                pointer0 += computer0.executeCommand(program[pointer0])
+            }
+            return@async
+        }
+        val suspend2 = async {
+            while (!(computer0.isWaiting && computer1.isWaiting) && pointer1 in program.indices) {
+                pointer1 += computer1.executeCommand(program[pointer1])
+            }
+            return@async
+        }
+
+        try {
+            awaitAll(suspend1, suspend2)
+        } catch (e: TimeoutCancellationException) {
+            //println(e.message)
+        }
+
+        counter
+    }
+
+    open class SoundComputer(protected val computerId: Long = 0) {
+
+        protected val register = mutableMapOf<Char, Long>()
 
         private val soundBuffer = mutableListOf<Long>()
 
         private val recovered = mutableListOf<Long>()
 
-        fun runUntilRecovered(program: List<Command>): Long {
+        open fun runUntilHalt(program: List<Command>): Long {
             var pointer = 0
             while (recovered.isEmpty() && pointer in program.indices) {
-                pointer += executeCommand(program[pointer])
+                pointer += executeCommandBlocking(program[pointer])
             }
             return soundBuffer.last()
         }
 
-        private fun valueOf(string: String): Long {
+        protected fun valueOf(string: String): Long {
             return if (string.length > 1) {
                 //Assume it is a number
                 string.toLong()
@@ -51,7 +98,7 @@ class Day18 : Day(2017,18) {
                 if (char.isDigit()) {
                     string.toLong()
                 } else {
-                    register.getOrDefault(char, 0)
+                    register.getOrDefault(char, computerId)
                 }
             }
         }
@@ -59,7 +106,7 @@ class Day18 : Day(2017,18) {
         /**
          * Returns the offset of the next command
          */
-        fun executeCommand(command: Command): Int {
+        open fun executeCommandBlocking(command: Command): Int {
             when (command.instruction) {
                 Instruction.SND -> {
                     soundBuffer.add(valueOf(command.x))
@@ -124,6 +171,32 @@ class Day18 : Day(2017,18) {
             MOD,
             RCV,
             JGZ
+        }
+    }
+
+    class DuetComputer(programId: Long, private val sendingChannel: Channel<Long>, private val receivingChannel: Channel<Long>, private val onSend: (() -> Unit)? = null): SoundComputer(programId) {
+        var isWaiting: Boolean = false
+            private set
+
+        suspend fun executeCommand(command: Command): Int {
+            return when (command.instruction) {
+                Instruction.SND -> {
+                    val toSend = valueOf(command.x)
+                    sendingChannel.send(toSend)
+                    onSend?.invoke()
+                    1
+                }
+                Instruction.RCV -> {
+                    isWaiting = true
+                    val received = withTimeout(1000) { receivingChannel.receive() }
+                    isWaiting = false
+                    register[command.x.first()] = received
+                    1
+                }
+                else -> {
+                    super.executeCommandBlocking(command)
+                }
+            }
         }
     }
 }
