64b71f0c66bc7577146cbc2d97eba991a6f29c30
heimmat <git.heiming@outlook.com>
2021-04-12 13:04:01 +0200

    Year 2017 Day 22 Part 2

--- src/main/kotlin/year2017/Day22.kt
+++ src/main/kotlin/year2017/Day22.kt
@@ -12,14 +12,27 @@ class Day22 : Day(2017,22) {
     )
 
     override fun part1(): Any {
-        val virusCarrier = VirusCarrier(input.asList.filterNotEmpty())
+        val virusCarrier = BasicCarrier(input.asList.filterNotEmpty())
         repeat(10_000) {
             virusCarrier.burst()
         }
         return virusCarrier.infected
     }
 
-    class VirusCarrier(initialMap: List<String>) {
+    override fun part2(): Any {
+        val carrier = EvolvedCarrier(input.asList.filterNotEmpty())
+        repeat(10_000_000) {
+            carrier.burst()
+        }
+        return carrier.infected
+    }
+
+    interface VirusCarrier {
+        fun burst() {}
+        val infected: Int
+    }
+
+    class BasicCarrier(initialMap: List<String>): VirusCarrier {
         init {
             if (initialMap.any { it.length != initialMap.size }) throw IllegalArgumentException()
         }
@@ -34,11 +47,11 @@ class Day22 : Day(2017,22) {
         var direction = Direction.UP; private set
         var position = 0 to 0; private set
 
-        var infected = 0; private set
+        override var infected = 0; private set
 
         val Pair<Int,Int>.isInfected: Boolean get() = map.getOrDefault(this, false)
 
-        fun burst() {
+        override fun burst() {
             val offset = if (position.isInfected) 1 else -1
             val directionValues = Direction.values()
             //println("Old direction is $direction")
@@ -52,6 +65,48 @@ class Day22 : Day(2017,22) {
             }
             position += direction.coordinateDiff
             //println("New position is $position")
+        }
+    }
+
+    class EvolvedCarrier(initialMap: List<String>): VirusCarrier {
+        val map: MutableMap<Pair<Int,Int>, InfectionState> = initialMap.flatMapIndexed { yIndex: Int, s: String ->
+            val y = yIndex - initialMap.size/2
+            s.mapIndexed { xIndex, c ->
+                val x = xIndex - initialMap.size/2
+                (x to y) to if (c == '#') InfectionState.INFECTED else InfectionState.CLEAN
+            }
+        }.toMap().toMutableMap()
+
+        var direction = Direction.UP; private set
+        var position = 0 to 0; private set
+
+        override var infected = 0; private set
+
+        val Pair<Int,Int>.isInfected: Boolean get() = map.getOrDefault(this, InfectionState.CLEAN) == InfectionState.INFECTED
+        val Pair<Int,Int>.infectionState: InfectionState get() = map.getOrDefault(this, InfectionState.CLEAN)
+
+        override fun burst() {
+            val offset = when (position.infectionState) {
+                InfectionState.CLEAN -> -1
+                InfectionState.WEAKENED -> 0
+                InfectionState.INFECTED -> 1
+                InfectionState.FLAGGED -> 2
+            }
+            val directionValues = Direction.values()
+            direction = directionValues[(direction.ordinal + directionValues.size + offset) % directionValues.size]
+            val newState = InfectionState.values()[(position.infectionState.ordinal + 1) % InfectionState.values().size]
+            if (newState == InfectionState.INFECTED) {
+                infected++
+            }
+            map[position] = newState
+            position += direction.coordinateDiff
+        }
+
+        enum class InfectionState {
+            CLEAN,
+            WEAKENED,
+            INFECTED,
+            FLAGGED
         }
     }
 }
