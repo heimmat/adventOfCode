160e9485a219483198e797de3c75b6d7432b0790
heimmat <git.heiming@outlook.com>
2021-11-19 11:06:49 +0100

    Year 2018 Day 07 Part 1

--- src/main/kotlin/year2018/Day07.kt
+++ src/main/kotlin/year2018/Day07.kt
@@ -11,17 +11,51 @@ class Day07 : Day(2018,7) {
         Step B must be finished before step E can begin.
         Step D must be finished before step E can begin.
         Step F must be finished before step E can begin.""".trimIndent().split("\n")
-    val inputSimplified = testInput.map {
+    val inputSimplified = input.asList.map {
         val split = it.split(" ")
         split[7].first() to split[1].first() }
 
 
-    val tasks = inputSimplified.groupBy { it.first }.map { it.key to it.value.map { it.second }.toSet() }.toMap()
+    val taskDependencyGraph = inputSimplified.groupBy { it.first }.map { it.key to it.value.map { it.second }.toSet() }.toMap()
+    //val tasksWithoutPredecessor = inputSimplified.toMap().values.filter { it !in inputSimplified.toMap().keys }
+    val highestChar = 'Z'
+    val charRange = 'A'..highestChar
+    val tasksWithoutPredecessor = charRange.filter { it !in inputSimplified.toMap().keys }
 
     override fun part1(): Any {
         //find task not in requirements
-        val firstTask = inputSimplified.toMap().values.first { it !in inputSimplified.toMap().keys }
-        return tasks
+        println("tasksWithoutPredecessor: $tasksWithoutPredecessor")
+        val finishedTasks = mutableSetOf<Char>()
+        val tasksWithFinishedPredecessor: () -> Set<Char> = {
+            taskDependencyGraph.filter { it.value.all { it in finishedTasks } }.keys.filterNot { it in finishedTasks }.toSet()
+        }
+        val availableTasks: () -> Set<Char> = {
+            (tasksWithFinishedPredecessor() + tasksWithoutPredecessor).filter { it !in finishedTasks }.toSet()
+        }
+        val remainingTasks: () -> Set<Char> = {
+            charRange.filterNot { it in finishedTasks || it in availableTasks() }.toSet()
+        }
+        val firstTask = tasksWithoutPredecessor.sorted().first()
+
+
+        println(taskDependencyGraph)
+        println(taskDependencyGraph.size)
+
+        var nextTask = firstTask
+
+        while (finishedTasks.size < charRange.count() - 1 && availableTasks().isNotEmpty()) {
+            println("nextTask: $nextTask")
+            finishedTasks.add(nextTask)
+            println("finishedTasks: $finishedTasks")
+            nextTask = availableTasks().sorted().first()
+            println("tasksWithFinishedPredecessor: ${tasksWithFinishedPredecessor()}")
+            println("availableTasks: ${availableTasks()}")
+            println("remainingTasks: ${remainingTasks()}")
+            println("$taskDependencyGraph\n")
+        }
+        finishedTasks.add(nextTask)
+        println(finishedTasks.size)
+        return finishedTasks.joinToString("")
 
     }
 
