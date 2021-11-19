0227086d6a2026834f200df97d0b734530d871f6
heimmat <git.heiming@outlook.com>
2021-11-19 14:19:40 +0100

    Year 2018 Day 07 prepare for Part 2

--- src/main/kotlin/year2018/Day07.kt
+++ src/main/kotlin/year2018/Day07.kt
@@ -3,6 +3,7 @@ package year2018
 import Day
 
 class Day07 : Day(2018,7) {
+    val debug = true
     val testInput = """
         Step C must be finished before step A can begin.
         Step C must be finished before step F can begin.
@@ -11,52 +12,61 @@ class Day07 : Day(2018,7) {
         Step B must be finished before step E can begin.
         Step D must be finished before step E can begin.
         Step F must be finished before step E can begin.""".trimIndent().split("\n")
-    val inputSimplified = input.asList.map {
+    val inputSimplified = (if (debug) testInput else input.asList).map {
         val split = it.split(" ")
         split[7].first() to split[1].first() }
 
 
     val taskDependencyGraph = inputSimplified.groupBy { it.first }.map { it.key to it.value.map { it.second }.toSet() }.toMap()
     //val tasksWithoutPredecessor = inputSimplified.toMap().values.filter { it !in inputSimplified.toMap().keys }
-    val highestChar = 'Z'
+    val highestChar = if (debug) 'F' else 'Z'
     val charRange = 'A'..highestChar
     val tasksWithoutPredecessor = charRange.filter { it !in inputSimplified.toMap().keys }
 
+    val finishedTasks = mutableSetOf<Char>()
+    val tasksWithFinishedPredecessor: () -> Set<Char> = {
+        taskDependencyGraph.filter { it.value.all { it in finishedTasks } }.keys.filterNot { it in finishedTasks }.toSet()
+    }
+    val availableTasks: () -> Set<Char> = {
+        (tasksWithFinishedPredecessor() + tasksWithoutPredecessor).filter { it !in finishedTasks }.toSet()
+    }
+    val remainingTasks: () -> Set<Char> = {
+        charRange.filterNot { it in finishedTasks || it in availableTasks() }.toSet()
+    }
+
     override fun part1(): Any {
         //find task not in requirements
-        println("tasksWithoutPredecessor: $tasksWithoutPredecessor")
-        val finishedTasks = mutableSetOf<Char>()
-        val tasksWithFinishedPredecessor: () -> Set<Char> = {
-            taskDependencyGraph.filter { it.value.all { it in finishedTasks } }.keys.filterNot { it in finishedTasks }.toSet()
-        }
-        val availableTasks: () -> Set<Char> = {
-            (tasksWithFinishedPredecessor() + tasksWithoutPredecessor).filter { it !in finishedTasks }.toSet()
-        }
-        val remainingTasks: () -> Set<Char> = {
-            charRange.filterNot { it in finishedTasks || it in availableTasks() }.toSet()
-        }
+//        println("tasksWithoutPredecessor: $tasksWithoutPredecessor")
         val firstTask = tasksWithoutPredecessor.sorted().first()
-
-
-        println(taskDependencyGraph)
-        println(taskDependencyGraph.size)
-
+//        println(taskDependencyGraph)
+//        println(taskDependencyGraph.size)
         var nextTask = firstTask
-
         while (finishedTasks.size < charRange.count() - 1 && availableTasks().isNotEmpty()) {
-            println("nextTask: $nextTask")
+//            println("nextTask: $nextTask")
             finishedTasks.add(nextTask)
-            println("finishedTasks: $finishedTasks")
+//            println("finishedTasks: $finishedTasks")
             nextTask = availableTasks().sorted().first()
-            println("tasksWithFinishedPredecessor: ${tasksWithFinishedPredecessor()}")
-            println("availableTasks: ${availableTasks()}")
-            println("remainingTasks: ${remainingTasks()}")
-            println("$taskDependencyGraph\n")
+//            println("tasksWithFinishedPredecessor: ${tasksWithFinishedPredecessor()}")
+//            println("availableTasks: ${availableTasks()}")
+//            println("remainingTasks: ${remainingTasks()}")
+//            println("$taskDependencyGraph\n")
         }
         finishedTasks.add(nextTask)
-        println(finishedTasks.size)
+//        println(finishedTasks.size)
         return finishedTasks.joinToString("")
 
     }
+
+    override fun part2(): Any {
+        val baseDuration = if (debug) 1 else 61
+        val workerCount = if (debug) 2 else 5
+        fun timeForTask(task: Char): Int {
+            return baseDuration + charRange.indexOf(task)
+        }
+
+        println("Time for task C: ${timeForTask('C')}")
+        return 1
+    }
+
 
 }
