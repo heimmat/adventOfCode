02fe190410877f9e17483b4a8a7a8e31fb8b04df
heimmat <git.heiming@outlook.com>
2021-12-06 14:39:29 +0100

    Year 2021 Day 5

--- src/test/kotlin/test2021/TestDay05.kt
+++ src/test/kotlin/test2021/TestDay05.kt
@@ -4,5 +4,5 @@ import TestDay
 import year2021.Day05
 
 class TestDay05: TestDay(Day05()) {
-    override val results: Pair<Any, Any> = 6189 to 0
+    override val results: Pair<Any, Any> = 6189 to 19164
 }
