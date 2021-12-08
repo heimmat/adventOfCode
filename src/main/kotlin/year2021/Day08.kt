7f0248e4a70b51089e9f3807732a2681052ab0f5
heimmat <git.heiming@outlook.com>
2021-12-08 06:57:52 +0100

    Year 2021 Day 08 Part 1

---
+++ src/main/kotlin/year2021/Day08.kt
@@ -0,0 +1,35 @@
+package year2021
+
+import Day
+
+class Day08(debug: Boolean = false): Day(2021,8, debug) {
+    private val testInput = """
+        be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+        edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+        fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+        fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+        aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+        fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+        dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+        bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+        egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+        gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+    """.trimIndent()
+
+    private val signals = (if (debug) testInput.split("\n") else input.asList)
+        .map {
+            val (input, output) = it.split(" | ")
+                .map {
+                    it.split(" ")
+                }
+            input to output
+        }
+
+    override fun part1(): Any {
+        return signals.sumOf {
+            it.second.count {
+                it.length in setOf(2,3,4,7)
+            }
+        }
+    }
+}
