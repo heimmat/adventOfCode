81ccb2d206103b42b018df11290f383e0ce86c57
heimmat <git.heiming@outlook.com>
2022-12-05 14:12:02 +0100

    add user-agent

--- src/main/kotlin/HttpClient.kt
+++ src/main/kotlin/HttpClient.kt
@@ -4,6 +4,6 @@ import java.io.File
 
 class HttpClient {
     private val session = File("src/main/resources/session.secret").readText()
-    private val defaultParameters = Pair("Cookie", "session=$session")
-    fun get(path: String) = path.httpGet().header(defaultParameters).responseString().second.body().asString("text/plain")
+    private val defaultParameters = arrayOf(Pair("Cookie", "session=$session"), Pair("User-Agent", "github.com/heimmat/adventOfCode by heimmat"))
+    fun get(path: String) = path.httpGet().header(*defaultParameters).responseString().second.body().asString("text/plain")
 }
